ceece4cd7bf3ffdd43f9771e2633cbdd7eed2614
fabiovalencio <fabiovalencio@hotmail.com>
2019-09-14 12:22:42 -0300

    change index page title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Docker Laravel
                 </div>
 
                 <div class="links">
